400b56291b8f53e6bd1fe560847bebbdda3bf8a2
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-18 12:47:17 +0200

    renamed main initial view

--- SwiftUI-SA-Appetizers/SwiftUI_SA_AppetizersApp.swift
+++ SwiftUI-SA-Appetizers/SwiftUI_SA_AppetizersApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct SwiftUI_SA_AppetizersApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            AppetizerTabView()
         }
     }
 }
