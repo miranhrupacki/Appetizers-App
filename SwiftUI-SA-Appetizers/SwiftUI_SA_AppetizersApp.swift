112d451404254cf7f1eb8632268309a18ece3a70
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-18 12:24:52 +0200

    Initial Commit

---
+++ SwiftUI-SA-Appetizers/SwiftUI_SA_AppetizersApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_SA_AppetizersApp.swift
+//  SwiftUI-SA-Appetizers
+//
+//  Created by Miran Hrupački on 18.06.2024..
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_SA_AppetizersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
