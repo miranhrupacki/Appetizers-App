76894f980bc37d9bef46662801327b517a749653
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-18 12:47:07 +0200

    tab view that holds child views

--- SwiftUI-SA-Appetizers/AppetizerTabView.swift
+++ SwiftUI-SA-Appetizers/AppetizerTabView.swift
@@ -9,9 +9,26 @@ import SwiftUI
 
 struct AppetizerTabView: View {
     var body: some View {
-        VStack {
-             
+        TabView {
+            AppetizerListView()
+                .tabItem {
+                    Image(systemName: "house")
+                    Text("Home")
+                }
+            
+            AccountView()
+                .tabItem {
+                    Image(systemName: "person")
+                    Text("Account")
+                }
+            
+            OrderView()
+                .tabItem {
+                    Image(systemName: "bag")
+                    Text("Order")
+                }
         }
+        .accentColor(.brandPrimary)
     }
 }
 
