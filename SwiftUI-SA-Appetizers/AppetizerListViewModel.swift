69938d55dbb8d2b2d0148861ba03071cbcefe4ae
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-19 10:47:23 +0200

    connected published data and bool for is loading, calling network and updating data

---
+++ SwiftUI-SA-Appetizers/AppetizerListViewModel.swift
@@ -0,0 +1,40 @@
+//
+//  AppetizerListViewModel.swift
+//  SwiftUI-SA-Appetizers
+//
+//  Created by Miran Hrupački on 18.06.2024..
+//
+
+import SwiftUI
+
+final class AppetizerListViewModel: ObservableObject {
+    // using published since we want to notify the StateObject for changes
+    @Published var appetizers: [Appetizer] = []
+    @Published var alertItem: AlertItem?
+    @Published var isLoading: Bool = false
+    
+    func getAppetizers() {
+        isLoading = true
+        
+        NetworkManager.shared.getAppetizers { result in
+            DispatchQueue.main.async {
+                self.isLoading = false
+                switch result {
+                case .success(let appetizers):
+                    self.appetizers = appetizers
+                case .failure(let error):
+                    switch error {
+                    case .invalidResponse:
+                        self.alertItem = AlertContext.invalidResponse
+                    case .invalidURL:
+                        self.alertItem = AlertContext.invalidURL
+                    case .invalidData:
+                        self.alertItem = AlertContext.invalidData
+                    case .unableToComplete:
+                        self.alertItem = AlertContext.unableToComplete
+                    }
+                }
+            }
+        }
+    }
+}
