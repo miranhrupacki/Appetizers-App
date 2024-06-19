4fcf2e915613f22cd65848a1aa2ccc7063fdd992
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-19 10:48:30 +0200

    added state object of view model, created ui with loader, connecting alert and updating list cell

--- SwiftUI-SA-Appetizers/AppetizerListView.swift
+++ SwiftUI-SA-Appetizers/AppetizerListView.swift
@@ -8,10 +8,32 @@
 import SwiftUI
 
 struct AppetizerListView: View {
+    // @StateObject is used for listening and when initialized right away, if passed through views, then use @ObservedObject
+    @StateObject private var viewModel = AppetizerListViewModel()
+    
     var body: some View {
-        NavigationView {
-            Text("Appetizer list view")
+        ZStack {
+            NavigationView {
+                List(viewModel.appetizers) { appetizer in
+                    AppetizerListCell(appetizer: appetizer)
+                }
                 .navigationTitle("🍟 Appetizers")
+            }
+            .onAppear {
+                viewModel.getAppetizers()
+            }
+            
+            if viewModel.isLoading {
+                ProgressView()
+                    .controlSize(.large)
+                    .progressViewStyle(.circular)
+                    .tint(.brandPrimary)
+            }
+        }
+        .alert(item: $viewModel.alertItem) { alertItem in
+            Alert(title: alertItem.title,
+                  message: alertItem.message,
+                  dismissButton: alertItem.dismissButton)
         }
     }
 }
