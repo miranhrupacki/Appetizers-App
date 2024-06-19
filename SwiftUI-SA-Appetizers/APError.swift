c94b569614f752f51b9eec795726a53ac765331c
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-19 10:43:59 +0200

    created error enum

---
+++ SwiftUI-SA-Appetizers/APError.swift
@@ -0,0 +1,15 @@
+//
+//  APError.swift
+//  SwiftUI-SA-Appetizers
+//
+//  Created by Miran Hrupački on 18.06.2024..
+//
+
+import Foundation
+
+enum APError: Error {
+    case invalidURL
+    case invalidResponse
+    case invalidData
+    case unableToComplete
+}
