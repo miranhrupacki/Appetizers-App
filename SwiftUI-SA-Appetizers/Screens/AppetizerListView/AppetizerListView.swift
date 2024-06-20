dd3a396fe2d979dbbbca535be5787ba6535fd94e
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-20 22:19:40 +0200

    removed data logic to view model, creating view of single item

--- SwiftUI-SA-Appetizers/Screens/AppetizerListView/AppetizerListView.swift
+++ SwiftUI-SA-Appetizers/Screens/AppetizerListView/AppetizerListView.swift
@@ -16,11 +16,21 @@ struct AppetizerListView: View {
             NavigationView {
                 List(viewModel.appetizers) { appetizer in
                     AppetizerListCell(appetizer: appetizer)
+                        .onTapGesture {
+                            viewModel.selectedAppetizer = appetizer
+                            viewModel.isShowingDetail = true
+                        }
                 }
                 .navigationTitle("🍟 Appetizers")
+                .disabled(viewModel.isShowingDetail)
             }
             .onAppear {
                 viewModel.getAppetizers()
+            }
+            .blur(radius: viewModel.isShowingDetail ? 20 : 0)
+            
+            if viewModel.isShowingDetail {
+                AppetizerDetailView(appetizer: viewModel.selectedAppetizer ?? MockData.sampleAppetizer, isShowingDetails: $viewModel.isShowingDetail)
             }
             
             if viewModel.isLoading {
