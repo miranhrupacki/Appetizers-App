46456c701b03dc291e1baae250b72eba610f427c
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-23 21:58:15 +0200

    created ui for account view with forms, calendar, toggle options and saving/updating app storage in view model

--- SwiftUI-SA-Appetizers/Screens/AccountView/AccountView.swift
+++ SwiftUI-SA-Appetizers/Screens/AccountView/AccountView.swift
@@ -8,10 +8,50 @@
 import SwiftUI
 
 struct AccountView: View {
+    @StateObject var viewModel = AccountViewModel()
+
     var body: some View {
         NavigationView {
-            Text("Account view") 
-                .navigationTitle("👨‍💼 Account")
+            Form {
+                Section(content: {
+                    TextField("First Name", text: $viewModel.user.firstName)
+                        .textInputAutocapitalization(.words)
+                    TextField("Last Name", text: $viewModel.user.lastName)
+                        .textInputAutocapitalization(.words)
+                    TextField("Email Name", text: $viewModel.user.email)
+                        .keyboardType(.emailAddress)
+                        .textInputAutocapitalization(.never)
+                        .autocorrectionDisabled()
+                    
+                    DatePicker("Birthday", selection: $viewModel.user.birthDate, displayedComponents: .date)
+                    
+                    Button {
+                        viewModel.saveChanges()
+                    } label: {
+                        Text("Save Changes")
+                    }
+                }, header: {
+                    Text("Personal Info")
+                })
+                
+                Section(content: {
+                    Toggle(isOn: $viewModel.user.extraNapkins, label: { Text("Extra Napkins") })
+                    Toggle(isOn: $viewModel.user.frequentRefills, label: { Text("Frequent Refills") })
+                }, header: {
+                    Text("Requests")
+                })
+                .toggleStyle(.switch)
+                .tint(.brandPrimary)
+            }
+            .navigationTitle("👨‍💼 Account")
+        }
+        .onAppear {
+            viewModel.retrieveUser()
+        }
+        .alert(item: $viewModel.alertItem) { alertItem in
+            Alert(title: alertItem.title,
+                  message: alertItem.message,
+                  dismissButton: alertItem.dismissButton)
         }
     }
 }
