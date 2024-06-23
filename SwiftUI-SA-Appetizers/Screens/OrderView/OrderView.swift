c990e3571b9838bced6fc29a37edcc0a9e42e1b0
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-23 22:02:52 +0200

    passed environment object variable, created ui for orders added to list, updating price and added possibility to remove items from list, handling empty state

--- SwiftUI-SA-Appetizers/Screens/OrderView/OrderView.swift
+++ SwiftUI-SA-Appetizers/Screens/OrderView/OrderView.swift
@@ -8,10 +8,36 @@
 import SwiftUI
 
 struct OrderView: View {
+    @EnvironmentObject var order: Order
+
     var body: some View {
         NavigationView {
-            Text("Order view")
-                .navigationTitle("🧾 Orders")
+            ZStack {
+                VStack {
+                    List {
+                        ForEach(order.items) { appetizer in
+                            AppetizerListCell(appetizer: appetizer)
+                        }
+                        .onDelete(perform: { indexSet in
+                            order.delete(at: indexSet)
+                        })
+                    }
+                    .listStyle(.plain)
+                    
+                    Button {
+                        print("order placed")
+                    } label: {
+                        Text("$\(order.totalPrice) - Place Order")
+                    }
+                    .modifier(StandardButtonStyle())
+                    .padding(.bottom, 25)
+                }
+                
+                if order.items.isEmpty {
+                    EmptyState(imageName: .emptyOrder, message: "You have no items in your order. Please add an appetizer.")
+                }
+            }
+            .navigationTitle("🍟 Appetizers")
         }
     }
 }
