ae9d0147ed1380021fffb67731ae235cec1189d9
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-23 22:03:49 +0200

    new cases added

--- SwiftUI-SA-Appetizers/Utilities/Alerts/Alert.swift
+++ SwiftUI-SA-Appetizers/Utilities/Alerts/Alert.swift
@@ -30,4 +30,20 @@ struct AlertContext {
     static let unableToComplete = AlertItem(title:  Text("Server Error"),
                                               message:  Text("Unable to complete your request at this time. Pleace check your internet connection."),
                                               dismissButton: .default(Text("OK")))
+    
+    static let invalidForm = AlertItem(title:  Text("Invalid Form"),
+                                              message:  Text("Please ensure all fields in the form have been filled out."),
+                                              dismissButton: .default(Text("OK")))
+    
+    static let invalidEmail = AlertItem(title:  Text("Invalid Email"),
+                                              message:  Text("Please ensure your email is correct."),
+                                              dismissButton: .default(Text("OK")))
+    
+    static let userSaveSuccess = AlertItem(title:  Text("Profile Saved"),
+                                              message:  Text("Your profile information was successfully saved."),
+                                              dismissButton: .default(Text("OK")))
+    
+    static let invalidUserData = AlertItem(title:  Text("Profile Error"),
+                                              message:  Text("There was an error saving or retreiving your profile."),
+                                              dismissButton: .default(Text("OK")))
 }
