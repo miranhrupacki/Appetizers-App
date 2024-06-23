7cddf3379185a4c5815fd175b1e5e22d120f5302
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-23 21:56:15 +0200

    model used for account screen, user info

--- SwiftUI-SA-Appetizers/Model/User.swift
+++ SwiftUI-SA-Appetizers/Model/User.swift
@@ -6,3 +6,14 @@
 //
 
 import Foundation
+
+struct User: Codable {
+    var firstName = ""
+    var lastName = ""
+    var email = ""
+    var birthDate = Date()
+    var extraNapkins = false
+    var frequentRefills = false
+    
+    
+}
