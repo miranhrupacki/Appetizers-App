05ce1c5b5d505541a83796fe4f7fb40ee17584f0
Miran Hrupački <miran.hrupacki@barrage.net>
2024-06-23 21:55:54 +0200

    order similar to view model, used for tracking added or deleted items, and total price

--- SwiftUI-SA-Appetizers/Model/Order.swift
+++ SwiftUI-SA-Appetizers/Model/Order.swift
@@ -5,4 +5,20 @@
 //  Created by Miran Hrupački on 21.06.2024..
 //
 
-import Foundation
+import SwiftUI
+
+final class Order: ObservableObject {
+    @Published var items: [Appetizer] = []
+    
+    var totalPrice: Double {
+        items.reduce(0) { $0 + $1.price }
+    }
+    
+    func add(_ appetizer: Appetizer) {
+        items.append(appetizer)
+    }
+    
+    func delete(at offsets: IndexSet) {
+        items.remove(atOffsets: offsets)
+    }
+}
